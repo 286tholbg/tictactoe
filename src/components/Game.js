604d20404483f35f357a48c14dc3c94473577359
286tholbg <107855431+286tholbg@users.noreply.github.com>
2022-11-08 23:01:56 +1100

    react game start and finish functions

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import { calculateWinner } from "../helpers";
 import Board from './Board'
 
+const styles = {
+    width: '200px',
+    margin: '20px auto'
+}
+
 const Game = () => {
 
     const [board, setBoard] = useState(Array(9).fill(null))
@@ -19,15 +24,22 @@ const Game = () => {
     }
 
     const jumpTo = () => {
-        
-    }
-
-    const renderMoves = () => {
 
     }
+
+    const renderMoves = () => ( <button onClick={() => setBoard(Array(9).fill(null))}>
+            Start Game
+        </button>
+    )
 
     return (
+        <>
         <Board squares={board} onClick={handleClick}/>
+        <div style={styles}>
+            <p>{winner ? 'Winner: ' + winner : 'Next Player: ' + (isNext ? 'X' : 'O')}</p>
+            {renderMoves()}
+        </div>
+        </>
     )
 }
 
