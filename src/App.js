e6b9c62504fb3fee6b29dc5b81569a76a3eadf28
286tholbg <107855431+286tholbg@users.noreply.github.com>
2022-11-08 22:05:06 +1100

    components board square and game added

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Game from "./components/Game";
+
+const App = () => <div><Game /></div>
+
+export default App;
---
+++ src/components/Game.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Game = () => <div>Tic Tac Toe Game</div>
+
+export default Game
