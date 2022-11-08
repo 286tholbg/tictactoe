4e598e61aa4490c0aa5b934a782b9a77085a3b3f
286tholbg <107855431+286tholbg@users.noreply.github.com>
2022-11-08 22:32:54 +1100

    added helper data and css

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,19 @@
 import React from "react";
 import Square from "./Square";
 
+const style = {
+    border: '4px solid darkblue',
+    borderRadius: '10px',
+    width: '250px',
+    height: '250px',
+    margin: '300px auto',
+    display: 'grid',
+    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+}
+
 const Board = ({ squares, onClick }) => {
     return (
-        <div>
+        <div style={style}>
             <Square value="1" onClick={() => onClick=('dummy value')} />
             <Square value="2" onClick={() => onClick=('dummy value')} />
             <Square value="3" onClick={() => onClick=('dummy value')} />
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+    background: 'lightblue',
+    border: '2px solid darkblue',
+    fontSize: '30px',
+    fontWeight: '800',
+    cursor: 'pointer',
+    outline: 'none'
+}
+
 const Square = ({ value, onClick }) => {
 
     return (
-        <button onClick={onClick}>
+        <button onClick={onClick} style={style}>
             {value}
         </button>
     )
